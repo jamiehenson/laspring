118de92a242b64b819845eb5ae20746cc43bfd5a
Jamie Henson <jhenson47@gmail.com>
2017-01-01 22:08:56 +0000

    More game stuff

--- assets/js/games/patterby.js
+++ assets/js/games/patterby.js
@@ -1,5 +1,5 @@
 var pGame = new Phaser.Game(worldX, worldY, Phaser.AUTO, 'patterby');
-var catIcon, pMenuFilter, pGameFilter;
+var catIcon, pMenuFilter, pGameFilter, pMenuTimer = 0, pMenuFlashText;
 
 var menuState = {
   preload: function() {
@@ -20,7 +20,11 @@ var menuState = {
     var icon = pGame.add.sprite(pGame.world.centerX, pGame.world.centerY, 'cat-sheet')
     var title = pGame.add.image(pGame.world.centerX, 10, 'pat-title');
     var desc = pGame.add.text(pGame.world.centerX, pGame.world.height - 10, "Pat as many pets as you can within the time, but only pat the right ones - some bite!",
-      {font: "18px Arcade", fill: "white", wordWrap: true, wordWrapWidth: pGame.world.width, align: "center"}
+      {font: "18px Arcade", fill: "white", wordWrap: true, wordWrapWidth: pGame.world.width, align: "center", backgroundColor: "#0000FF", padding: 10 }
+    );
+    desc.lineSpacing = 5;
+    pMenuFlashText = pGame.add.text(pGame.world.centerX, pGame.world.centerY, "TAP TO      START!",
+      {font: "30px Arcade", fill: "white", wordWrap: true, wordWrapWidth: pGame.world.width, align: "center"}
     );
 
     pGame.add.tween(icon.scale).to({x: 1.25, y: 1.25}, 3000, Phaser.Easing.Linear.None, true, 0, 1000, true);
@@ -30,27 +34,32 @@ var menuState = {
     icon.anchor.set(0.5);
     title.anchor.set(0.5, 0);
     desc.anchor.set(0.5, 1);
+    pMenuFlashText.anchor.set(0.5);
+
+    pGame.input.onTap.addOnce(this.start, this);
   },
 
   update: function() {
+    pMenuTimer += pGame.time.elapsed;
+    if ( pMenuTimer >= 1000 ) {
+      pMenuTimer -= 1000;
+      pMenuFlashText.visible = !pMenuFlashText.visible;
+    }
     pMenuFilter.update();
+  },
+
+  start: function() {
+    pGame.state.start('play');
   }
 };
 
-var unusedPets = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-var activePets = [];
-var goodPets = [];
-var badPets = [];
-var zones = [];
-var score = 0;
-var scoreText = "";
-var endTime;
-var timeLeft = 30000;
-var timeText = "";
+var unusedPets, activePets, goodPets, badPets, zones, goodCount, badCount,
+  score = 0, scoreText = "", endTime, timeLeft, timeText = "", circles,
+  animals = [], endText, endSubText;
 
 var playState = {
   preload: function() {
-    pGame.load.spritesheet('animal-sheet', 'assets/images/games/pat/animal-sheet.png', 128, 128);
+    pGame.load.spritesheet('animal-sheet', 'assets/images/games/pat/animals.png', 80, 80);
     pGame.load.shader('pGameFilter', 'assets/js/games/swirl-filter.frag');
     pGame.time.advancedTiming = true;
   },
@@ -74,30 +83,39 @@ var playState = {
     ui.drawRect(150, 0, pGame.world.width, 40);
     ui.alpha = 0.75;
 
-    for (var i = 0; i < 3; i++) {
-      for (var j = 0; j < 3; j++) {
-        var bgColour = "0x";
-        bgColour += Math.random().toString(16).slice(2, 8).toUpperCase();
-        ui.beginFill(bgColour, 1);
-        ui.drawCircle((130 * i) + 240, (100 * j) + 100, 90);
-      }
-    }
-
     timeText = pGame.add.text(160, 10, "Time: " + (timeLeft / 1000),
       {font: "18px Arcade", fill: "white", wordWrap: true, wordWrapWidth: pGame.world.width, align: "right"}
     );
     scoreText = pGame.add.text(pGame.world.width - 10, 10, "Score: " + score,
       {font: "18px Arcade", fill: "white", wordWrap: true, wordWrapWidth: pGame.world.width, align: "right"}
     );
+    timeLeft = 60000;
 
     scoreText.anchor.set(1, 0);
 
     this.generatePets();
 
-    endTime = pGame.time.now + 30000;
+    endTime = pGame.time.now + timeLeft;
+  },
+
+  generateCircles: function() {
+    circles = pGame.add.graphics(0, 0);
+
+    for (var j = 0; j < 3; j++) {
+      for (var i = 0; i < 3; i++) {
+        var bgColour = "0x";
+        bgColour += Math.random().toString(16).slice(2, 8).toUpperCase();
+        circles.lineStyle(2, 0xFFFF00, 1);
+        circles.beginFill(bgColour, 1);
+        circles.drawCircle((130 * i) + 240, (110 * j) + 95, 100);
+      }
+    }
   },
 
   generatePets: function(){
+    this.resetAnimalArrays();
+    this.generateCircles();
+
     var pet = pGame.add.text(10, 10, "PAT!",
       {font: "18px Arcade", fill: "white", wordWrap: true, wordWrapWidth: pGame.world.width, align: "center"}
     );
@@ -110,20 +128,34 @@ var playState = {
     this.drawPets();
   },
 
+  resetAnimalArrays: function() {
+    unusedPets = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+    activePets = [];
+    goodPets = [];
+    badPets = [];
+    goodCount = 0;
+    badCount = 0;
+    zones = 9;
+  },
+
   drawPets: function() {
     for (var j = 0; j < 3; j++) {
       for (var i = 0; i < 3; i++) {
-        var animal = pGame.add.sprite((i * 130) + 240, (j * 100) + 100, 'animal-sheet')
+        var animal = pGame.add.sprite((i * 130) + 240, (j * 110) + 95, 'animal-sheet')
         var chosenFrame = Math.floor(Math.random() * activePets.length);
         animal.frame = activePets[chosenFrame];
-        animal.width = 70;
-        animal.height = 70;
         animal.anchor.set(0.5);
         animal.inputEnabled = true;
         animal.input.userHandCursor = true;
         animal.key = activePets[chosenFrame]
         animal.events.onInputDown.add(this.animalPressed, this);
-        zones.push(animal);
+        if (goodPets.indexOf(animal.key) != -1) {
+          goodCount += 1;
+        }
+        if (badPets.indexOf(animal.key) != -1) {
+          badCount += 1;
+        }
+        animals.push(animal);
       }
     }
   },
@@ -131,10 +163,24 @@ var playState = {
   animalPressed: function(animal) {
     if (goodPets.indexOf(animal.key) != -1) {
       score += 50;
+      endTime += 500;
+      goodCount -= 1;
     } else {
       endTime -= 1000;
+      badCount -= 1;
     }
+
+    zones -= 1;
+
+    animal.destroy();
     scoreText.text = "Score: " + score;
+
+    if (goodCount <= 0 || zones <= 0) {
+      animals.forEach( function(animal) {
+        animal.destroy();
+      });
+      this.generatePets();
+    }
   },
 
   animalGenerationLoop: function(xmod, ymod, good) {
@@ -145,9 +191,10 @@ var playState = {
         animal.frame = unusedPets[chosenFrame];
         animal.width = 64;
         animal.height = 64;
+        animals.push(animal);
         activePets.push(unusedPets[chosenFrame]);
         if (good) {
-          goodPets.push(unusedPets[chosenFrame])
+          goodPets.push(unusedPets[chosenFrame]);
         } else {
           badPets.push(unusedPets[chosenFrame]);
         }
@@ -167,14 +214,29 @@ var playState = {
     }
   },
 
-  restart: function() {
+  endGame: function(){
+    pGame.paused = true;
+    endText = pGame.add.text(pGame.world.centerX, 10, "You got " + score + " points!\n\nThat's " + score / 50 + " critters who felt the love.",
+      {font: "36px Arcade", fill: "white", wordWrap: true, wordWrapWidth: pGame.world.width, align: "center", backgroundColor: "#0000FF"}
+    );
+    endSubText = pGame.add.text(pGame.world.centerX, pGame.world.height - 10, "Tap to go again!",
+      {font: "18px Arcade", fill: "white", wordWrap: true, wordWrapWidth: pGame.world.width, align: "center", backgroundColor: "#0000FF"}
+    );
+    endText.anchor.set(0.5, 0);
+    endSubText.anchor.set(0.5, 1);
+
+    pGame.input.onTap.addOnce(this.restart, this);
   },
 
-  endGame: function(){
-    console.log("over");
+  restart: function() {
+    pGame.paused = false;
+    timeLeft = 60000;
+    endText.kill();
+    endSubText.kill();
+    this.generatePets();
   }
 }
 
 pGame.state.add('menu', menuState);
 pGame.state.add('play', playState);
-pGame.state.start('play');
+pGame.state.start('menu');
